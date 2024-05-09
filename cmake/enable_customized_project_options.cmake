ec7524dc8070419eadb28fe504811840688f57d7
FeignClaims <www4112379@163.com>
2024-05-09 12:05:58 +0800

    Setup cmake framework

---
+++ cmake/enable_customized_project_options.cmake
@@ -0,0 +1,133 @@
+# - Customization point for project_options
+# This module sets all customization on dynamic_project_options
+#
+# Include this module right after `project()` to make use
+#
+# If conan2 or vcpkg is requried, include(fetch_project_options) separately and invoke `run_conan2()` or `run_vcpkg()`
+include_guard()
+
+include(${CMAKE_CURRENT_LIST_DIR}/fetch_project_options.cmake)
+
+# compile_commands.json
+set(ENABLE_COMPILE_COMMANDS_SYMLINK_DEFAULT ON)
+
+# hardening
+set(ENABLE_CONTROL_FLOW_PROTECTION_DEFAULT ON)
+set(ENABLE_ELF_PROTECTION_DEFAULT OFF)
+set(ENABLE_OVERFLOW_PROTECTION_DEFAULT ON)
+set(ENABLE_RUNTIME_SYMBOLS_RESOLUTION_DEFAULT ON)
+if(WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  set(ENABLE_STACK_PROTECTION_DEFAULT OFF)
+else()
+  set(ENABLE_STACK_PROTECTION_DEFAULT ON)
+endif()
+
+# optimization
+set(ENABLE_INTERPROCEDURAL_OPTIMIZATION_DEFAULT ON)
+set(ENABLE_NATIVE_OPTIMIZATION_DEFAULT ON)
+
+dynamic_project_options(
+  PREFIX
+  "starter" # set a prefix in case this project is used as a subproject
+
+  MSVC_WARNINGS
+  /W4 # Baseline reasonable warnings
+  /permissive- # standards conformance mode for MSVC compiler
+  /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
+  /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
+  /w14263 # 'function': member function does not override any base class virtual member function
+  /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not be destructed correctly
+  /w14287 # 'operator': unsigned/negative constant mismatch
+  /w14296 # 'operator': expression is always 'boolean_value'
+  /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
+  /w14545 # expression before comma evaluates to a function which is missing an argument list
+  /w14546 # function call before comma missing argument list
+  /w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
+  /w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
+  /w14555 # expression has no effect; expected expression with side- effect
+  /w14619 # pragma warning: there is no warning number 'number'
+  /w14640 # Enable warning on thread un-safe static member initialization
+  /w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior
+  /w14905 # wide string literal cast to 'LPSTR'
+  /w14906 # string literal cast to 'LPWSTR'
+  /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+  /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside the for-loop scope
+
+  CLANG_WARNINGS
+  -Wall
+  -Wextra # reasonable and standard
+  -Wcast-align # warn for potential performance problem casts
+  -Wcast-function-type # warn about function typemismatches in casts
+  -Wconversion # warn on type conversions that may lose data
+  -Wcovered-switch-default # warn if use default labels in fully covered switches over enumerations. This helps when a new enum value is added
+  -Wdouble-promotion # warn if float is implicit promoted to double
+  -Wextra-semi # warn about semicolon after in-class function definition
+  -Wfloat-equal # warn on comparing floating point with == or !=
+  -Wformat=2 # warn on security issues around functions that format output (ie printf)
+  # -Wglobal-constructors  # warn on declare global or static variables with dynamic constructors
+  -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+  -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
+  -Wmissing-noreturn # warn about functions that might be candidates for [[noreturn]]
+  -Wno-braced-scalar-init # todo: remove this when it is correct
+  -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps catch hard to track down memory errors
+  -Wnull-dereference # warn if a null dereference is detected
+  -Wold-style-cast # warn for c-style casts
+  -Woverloaded-virtual # warn if you overload (not override) a virtual function
+  -Wpacked # warn if a structure is given the packed attribute, but the packed attribute has no effect on the layout or size of the structure
+  -Wpedantic # warn if non-standard C++ is used
+  -Wpointer-arith # warn on pointer arithmetic
+  -Wshadow # warn the user if a variable declaration shadows one from a parent context
+  -Wsign-conversion # warn on sign conversions
+  -Wtautological-compare # warn for tautological comparisons
+  -Wthread-safety # warn for thread safety
+  -Wundef # warn if an undefined identifier is evaluated in an #if directive
+  -Wunreachable-code-aggressive # warn if code will never be executed
+  -Wunused # warn on anything being unused
+  -Wno-gnu-line-marker # avoid the warn on gnu line marker when `--save-temps=obj` enabled
+  -Wno-unused-command-line-argument # disable warn if command line argument unused
+  -ftemplate-backtrace-limit=0
+  -fconstexpr-backtrace-limit=0
+
+  GCC_WARNINGS
+  -Wall
+  -Wextra # reasonable and standard
+  -Wcast-align # warn for potential performance problem casts
+  -Wconversion # warn on type conversions that may lose data
+  -Wdisabled-optimization # warn if a requested optimization pass is disabled. often, the problem is that your code is too big or too complex
+  -Wdouble-promotion # warn if float is implicit promoted to double
+  -Wduplicated-branches # warn if if / else branches have duplicated code
+  -Wduplicated-cond # warn if if / else chain has duplicated conditions
+  -Wextra-semi # warn about semicolon after in-class function definition
+  -Wfloat-equal # warn on comparing floating point with == or !=
+  -Wformat=2 # warn on security issues around functions that format output (ie printf)
+  -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+  -Winvalid-pch # warn if a precompiled header is found in the search path but cannot be used
+  -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+  -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
+  -Wmissing-format-attribute # Warn about function pointers that might be candidates for format attributes
+  -Wmissing-include-dirs # warn if a user-supplied include directory does not exist
+  -Wmissing-noreturn # warn about functions that might be candidates for [[noreturn]]
+  -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps catch hard to track down memory errors
+  -Wnull-dereference # warn if a null dereference is detected
+  -Wold-style-cast # warn for c-style casts
+  -Woverloaded-virtual # warn if you overload (not override) a virtual function
+  -Wpedantic # warn if non-standard C++ is used
+  -Wpointer-arith # warn on pointer arithmetic
+  -Wredundant-decls # warn if anything is declared more than once in the same scope, even in cases where multiple declaration is valid and changes nothing
+  -Wshadow # warn the user if a variable declaration shadows one from a parent context
+  -Wsign-conversion # warn on sign conversions
+  -Wundef # warn if an undefined identifier is evaluated in an #if directive
+  -Wunused # warn on anything being unused
+  -Wuseless-cast # warn if you perform a cast to the same type
+  -Wno-unused-command-line-argument # disable warn if command line argument unused
+
+  CPPCHECK_OPTIONS
+  --enable=style,performance,warning,portability
+  --inline-suppr
+  --suppress=cppcheckError # We cannot act on a bug/missing feature of cppcheck
+  --suppress=internalAstError
+  --suppress=unmatchedSuppression # if a file does not have an internalAstError, we get an unmatchedSuppression error
+  --suppress=passedByValue
+  --suppress=syntaxError
+  --inconclusive
+)
---
+++ conanfile.py
@@ -0,0 +1,128 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, rm, rmdir
+from conan.tools.scm import Version
+import os
+
+
+class StarterRecipe(ConanFile):
+    name = "starter"
+    description = "A CMake template using Conan 2"
+    license = "Unlicense"
+    url = "https://github.com/FeignClaims/cmake_starter_template"
+    homepage = "https://github.com/FeignClaims/cmake_starter_template"
+    topics = ("template")
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    version = "0.0.1"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self._strict_options_requirements()
+
+    def layout(self):
+        # By default, distinguish configuraiotns by compiler name
+        # This can be changed by setting `tools.cmake.cmake_layout:build_folder_vars` in command-line or profiles
+        self.folders.build_folder_vars = ["settings.compiler"]
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("fmt/10.2.1")
+        self.requires("ms-gsl/4.0.0")
+        self.requires("range-v3/0.12.0")
+
+    @property
+    def _min_cppstd(self):
+        return 20
+
+    # In case the project requires C++14/17/20/... the minimum compiler version should be listed
+    @property
+    def _compilers_minimum_version(self):
+        return {"msvc": "193",
+                "gcc": "11",
+                "clang": "13",
+                "apple-clang": "14"}
+
+    def _validate_cppstd(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            # Validate the minimum cpp standard supported when installing the package. For C++ projects only
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+
+    @property
+    def _required_options(self):
+        options = []
+        # Usage: options.append(("boost", [("without_graph", False), ("without_test", False)]))
+        return options
+
+    def _strict_options_requirements(self):
+        for requirement, options in self._required_options:
+            for option_name, value in options:
+                setattr(self.options[requirement], f"{option_name}", value)
+
+    def _validate_options_requirements(self):
+        for requirement, options in self._required_options:
+            is_missing_option = not all(self.dependencies[requirement].options.get_safe(
+                f"{option_name}") == value for option_name, value in options)
+            if is_missing_option:
+                raise ConanInvalidConfiguration(
+                    f"{self.ref} requires {requirement} with these options: {options}")
+
+    def validate(self):
+        self._validate_cppstd()
+        self._validate_options_requirements()
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.25 <4.0.0]")
+        self.test_requires("catch2/3.6.0")
+
+    def generate(self):
+        CMakeDeps(self).generate()
+        toolchain = CMakeToolchain(self)
+        toolchain.presets_prefix = ""
+        toolchain.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        if not self.conf.get("tools.build:skip_test", default=False):
+            cmake.ctest(cli_args=["--rerun-failed", "--output-on-failure"])
+
+    def package(self):
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
+        cmake.install()
+
+        # some files extensions and folders are not allowed. Please, read the FAQs to get informed.
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "lib"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
+
+    def package_info(self):
+        self.cpp_info.libs = ["starter"]
+
+        self.cpp_info.set_property("cmake_file_name", "starter")
+        self.cpp_info.set_property("cmake_target_name", "starter::starter")
